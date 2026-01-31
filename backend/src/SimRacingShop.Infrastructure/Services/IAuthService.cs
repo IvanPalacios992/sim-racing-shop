4f97d64b3ce6638187df1745f1e764c77838cde8
navy992 <ivanpalacios@gnoss.com>
2026-01-31 14:15:34 +0100

    feat: #1 Servicio y controlador de Auth
    Añadido:
    -Interfaz y servicio de autorización, encargado de validar los credenciales el usuario, generar el token y registrar nuevos usuarios.
    - Controlador de autorización que permite registrar un usuario y hacer login así como algunos endpoint de prueba para comprobar que funcionan los endpoint privados y el los roles

---
+++ backend/src/SimRacingShop.Infrastructure/Services/IAuthService.cs
@@ -0,0 +1,14 @@
+﻿using SimRacingShop.Core.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimRacingShop.Infrastructure.Services
+{
+    public interface IAuthService
+    {
+        Task<AuthResponseDto> RegisterAsync(RegisterDto dto);
+        Task<AuthResponseDto> LoginAsync(LoginDto dto);
+        Task<UserDto?> GetUserByIdAsync(Guid userId);
+    }
+}
